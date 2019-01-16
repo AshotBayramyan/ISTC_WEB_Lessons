993cc171bf5862bf2db47e98e569263c3c7b3e8a
Hasmik Poghosyan <hasmikpoghosyan@Hasmiks-MBP.Dlink>
2019-01-16 22:14:27 +0400

    fhg

---
+++ WEB Homework/JS/9.js
@@ -0,0 +1,15 @@
+function showHint(str) {
+    if (str.length == 0) { 
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        }
+        xmlhttp.open("GET", "http://www.mocky.io/v2/5c3f73d5350000b02dec3987", true);
+        xmlhttp.send();
+    }
+}
