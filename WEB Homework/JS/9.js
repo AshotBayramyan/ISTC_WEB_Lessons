d13597953b725c43f6477b525b34044988a6e0d7
AshotBayramyan <ashotbayramyan@gmail.com>
2019-01-17 20:06:03 +0400

    asdfghjkl;'\

--- WEB Homework/JS/9.js
+++ WEB Homework/JS/9.js
@@ -1,13 +1,6 @@
-var name = document.getElementById("name");
-// var surname = document.getElementById("surname");
-var email = document.getElementById("email");
-var but = document.getElementById("button");
-
-submitButton.addEventListener("click", function(event){
-    var textName= name.value;
-    // var textName= surname.value;
-    var textEmail = email.value;
-   
-  document.getElementById("result").innerHTML ="Information about your profile; </b>" + "<b><br/> Your Name Is </b>" + textName 
-   + textEmail + "<b><br/> Your Address Is </b>";
+document.getElementById("but").addEventListener("click", function(){
+  var name = document.getElementsByClassName("inp");
+    for (let i = 0; i< name.length; i++) {
+      document.getElementById('txtHint').innerHTML += name[i].value + '<br>'; 
+    }
 })
